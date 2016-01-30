f76c17268304fbf7ea6480362ff6f04ad1006789
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-30 12:23:08 -0500

    init

---
+++ kitchingroup-57.el
@@ -0,0 +1,65 @@
+;;; kitchingroup-57.el --- preprint http://dx.doi.org/10.1021/acscatal.5b00538.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  John Kitchin
+
+;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
+;; Keywords:
+;; Package-Requires: ((cappa "0"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Preprint and supporting information for
+;; @article{kitchin-2015-examp,
+;;   author =	 {Kitchin, John R.},
+;;   title =	 {Examples of Effective Data Sharing in Scientific Publishing},
+;;   journal =	 {ACS Catalysis},
+;;   volume =	 {5},
+;;   number =	 {6},
+;;   pages =	 {3894-3899},
+;;   year =	 2015,
+;;   doi =		 {10.1021/acscatal.5b00538},
+;;   url =		 { http://dx.doi.org/10.1021/acscatal.5b00538 },
+;;   eprint =	 { http://dx.doi.org/10.1021/acscatal.5b00538 },
+;; }
+
+;;; Code:
+(require 'cappa)
+
+(cappa 'kitchingroup-57
+       :journal "ACS Catalysis"
+       :author '("John R. Kitchin")
+       :title "Examples of Effective Data Sharing in Scientific Publishing"
+       :volume "5"
+       :number "6"
+       :pages "3894-3899"
+       :year 2015
+       :doi "10.1021/acscatal.5b00538"
+       :url "http://dx.doi.org/10.1021/acscatal.5b00538"
+       :bibtex "@article{kitchin-2015-examp,
+  author =	 {Kitchin, John R.},
+  title =	 {Examples of Effective Data Sharing in Scientific Publishing},
+  journal =	 {ACS Catalysis},
+  volume =	 {5},
+  number =	 {6},
+  pages =	 {3894-3899},
+  year =	 2015,
+  doi =		 {10.1021/acscatal.5b00538},
+  url =		 { http://dx.doi.org/10.1021/acscatal.5b00538 },
+  eprint =	 { http://dx.doi.org/10.1021/acscatal.5b00538 },
+}")
+
+(provide 'kitchingroup-57)
+;;; kitchingroup-57.el ends here
